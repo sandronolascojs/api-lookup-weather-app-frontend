56fd189ac00bc3e6ec7ff1383e660866eedb3cda
Sandro Nolasco <sandronolasco1@gmail.com>
2022-08-31 21:13:19 -0300

    [FIX] API link

--- src/api/ip.js
+++ src/api/ip.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URI = 'http://api-lookup-weather-back.up.railway.app/api/' 
+const URI = 'https://api-lookup-weather-back.up.railway.app/api/' 
 
 export const getUserIp = async () => {
   try {
